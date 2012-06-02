300abc5660a26b46d8e51f31baf5c9299ea958a4
SlackerCoder <mario.folino@gmail.com>
2012-06-01 21:22:22 -0400

    Try again/

--- HelloWorld/IService1.cs
+++ HelloWorld/IService1.cs
@@ -8,6 +8,7 @@ using System.Text;
 
 namespace HelloWorld
 {
+    //asdasdasdas
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
